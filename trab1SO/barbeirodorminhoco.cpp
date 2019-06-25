d03e372f24bf1d4aa3dd9632c8030a84e57df7c1
Ingrid Santos <ils@cesar.org.br>
2019-06-25 19:10:33 -0400

    Trabalho 1 - 2019/1

---
+++ trab1SO/barbeirodorminhoco.cpp
@@ -0,0 +1,102 @@
+/*  TP1  Sistemas Operacionais
+    Arthur Bindá
+    Eduardo Lopes
+    Ingrid Santos
+    Execução: g++ barbeirodorminhoco.cpp -pthread -o barbeiros && ./barbeiros
+*/
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define N_CLIENTES 20
+#define N_CADEIRAS 5 
+#define N_BARBEIROS 3
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+sem_t sem_cadeiras;                         // cadeiras de espera
+sem_t sem_cad_barbeiro[N_BARBEIROS];        // cadeiras dos barbeiros
+sem_t sem_corte_cabelo[N_BARBEIROS];        // corte de cabelo feito
+sem_t sem_cliente_cadeira[N_BARBEIROS];     // sem fila de espera
+
+sem_t sem_escreve_visor, sem_le_visor;
+int visor;
+
+void* func_barbeiro(void *v) {
+    int id = * (int*) v;
+    while(1) {
+        sem_wait(&sem_escreve_visor);       // espera ter cliente
+        visor = id;                         // avisa que ta livre
+        sem_post(&sem_le_visor);            // avisa que ta acordado
+        sem_wait(&sem_cliente_cadeira[id]); // espera cliente alocar na cadeira
+        sleep(random()%1000+1);             // realiza atendimento
+        sem_post(&sem_corte_cabelo[id]);    // avisa que terminou de cortar
+        printf(ANSI_COLOR_CYAN "[BAR]->" ANSI_COLOR_RESET " Barbeiro %d cortou cabelo de um cliente. \n", id);  
+    }
+    return NULL;
+}
+
+void* func_cliente(void *v) {
+    int id = *(int*) v;
+    int minha_cadeira;
+
+    sleep(random()%1000+1);
+    if (sem_trywait(&sem_cadeiras) == 0) {                      // tenta entrar e sentar 
+        printf(ANSI_COLOR_GREEN "[CLI]->" ANSI_COLOR_RESET " Cliente %d entrou na barbearia.\n", id);        
+        sem_wait(&sem_le_visor);                                // espera algum barbeiro indicar que ta dormindo
+        minha_cadeira = visor;                                  // ve qual barbeiro indicou que ta dormindo
+        sem_post(&sem_escreve_visor);                           // avisa que ta pronto pra ser atendido
+        sem_wait(&sem_cad_barbeiro[minha_cadeira]);             // espera barbeiro acordar e ser atendido
+        sem_post(&sem_cliente_cadeira[minha_cadeira]);          // avisa que ta na cadeira
+        printf(ANSI_COLOR_YELLOW "[CLI]->" ANSI_COLOR_RESET " Cliente %d sentou na cadeira do barbeiro %d. \n", id, minha_cadeira);
+        sem_post(&sem_cadeiras);                                // deixa cadeira
+        sem_wait(&sem_corte_cabelo[minha_cadeira]);             // aguarda barbeiro finalizar o corte
+        sem_post(&sem_cad_barbeiro[minha_cadeira]);             // deixa cadeira do barbeiro
+        printf(ANSI_COLOR_RED "[CLI]->" ANSI_COLOR_RESET " Cliente %d deixou a barbearia.\n", id);
+    } else {
+        printf(ANSI_COLOR_BLUE "[CLI]->" ANSI_COLOR_RESET " Cliente %d não entrou na barbearia.\n", id);    // nao tem cadeiras vagas
+    }
+    return NULL;
+}
+
+int main(int argc, char const *argv[]) {
+    pthread_t thread_clientes[N_CLIENTES], thread_barbeiros[N_BARBEIROS];
+    int i, id_clientes[N_CLIENTES], id_barbeiros[N_BARBEIROS];
+
+    sem_init(&sem_cadeiras, 0, 5);              // cadeiras
+    sem_init(&sem_escreve_visor, 0, 1);
+    sem_init(&sem_le_visor, 0, 0);
+    
+    // inicia semáforos
+    for (i = 0; i < N_BARBEIROS; i++) {
+        sem_init(&sem_cad_barbeiro[i], 0, 1);
+        sem_init(&sem_cliente_cadeira[i], 0, 0);
+        sem_init(&sem_corte_cabelo[i], 0, 0);
+    }
+    
+    // cria os clientes
+    for (i = 0; i < N_CLIENTES; i++) {
+        id_clientes[i] = i;
+        pthread_create(&thread_clientes[i], NULL, func_cliente, (void*) &id_clientes[i]);
+    }
+
+    // cria os barbeiros
+    for (i = 0; i < N_BARBEIROS; i++) {
+        id_barbeiros[i] = i;
+        pthread_create(&thread_barbeiros[i], NULL, func_barbeiro, (void*) &id_barbeiros[i]);
+    }    
+    
+    // encerra os clientes
+    for (i = 0; i < N_CLIENTES; i++) {
+        pthread_join(thread_clientes[i], NULL);  
+    }
+    
+    return 0;
+}
---
+++ trab1SO/version2.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <cstdio>
+#include <pthread.h>
+#include <unistd.h>
+#include <mutex>
+#include <time.h>
+#include <ctime>    // For time()
+#include <cstdlib>  // For srand() and rand()
+#include <semaphore.h>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+#define STOCK_SIZE 5
+#define DEFAULT_AMOUNT 40
+#define STORES 12 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+time_t rawtime;
+struct tm * timeinfo;
+static sem_t stock_semaphore;
+static mutex stock_mutex;
+
+void time_now() {
+  time (&rawtime);
+  timeinfo = localtime(&rawtime);
+}
+
+struct Product {
+  int id;
+  int amount;
+};
+
+Product stock[STOCK_SIZE];
+
+void initStock() {
+  for (int i=0; i < STOCK_SIZE; ++i) {
+    stock[i].id = i;
+    stock[i].amount = DEFAULT_AMOUNT;
+  }
+}
+
+void printStock() {
+  printf("--------AZEITE STOCK--------\n");
+  for (int i=0; i < STOCK_SIZE; ++i) {
+    printf("Product [%d]\tAmount: %d\n", stock[i].id, stock[i].amount);
+  }
+}
+
+struct Thread_Args {
+  long thread_id;
+  int product_id = -1; // -1 if is not to read
+  int quantity;
+};
+
+void *reader(void *args) {
+  Thread_Args arguments;
+  arguments = *(struct Thread_Args*)args;
+  
+  long thread_id = arguments.thread_id;
+  int product_id = arguments.product_id;
+  time_now();
+  // Thread start
+  
+  printf("+++ Thread OF READ tid[%ld] invoked!! %d:%d:%d\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec);
+  // sem_wait(&stock_semaphore);
+  stock_mutex.lock();
+
+  printf(ANSI_COLOR_GREEN "[SYS][READ]->" ANSI_COLOR_RESET "TID[%ld] starting!!\n\t** ENTERING CRITIC REGION !!** %d:%d:%d\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec);
+  sleep(2);
+  
+  time_now();
+  printf(ANSI_COLOR_YELLOW "[RUNNING][READ]" ANSI_COLOR_RESET "->TID[%ld] %d:%d:%d\n\tReading data from PRODUCT_ID=[%d]. AMOUNT=[%d]\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec, product_id, stock[product_id].amount);
+
+  time_now();
+  printf(ANSI_COLOR_RED "[SYS][READ]" ANSI_COLOR_RESET "Thread OF READ tid[%ld] finished!!\n\t** LEFTING  CRITIC REGION !!** %d:%d:%d\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec);
+  
+  // sem_post(&stock_semaphore);
+  stock_mutex.unlock();
+  
+  pthread_exit(NULL);  
+}
+
+void *writer(void *args) {
+  srand(time(NULL));
+  Thread_Args arguments;
+  arguments = *(struct Thread_Args*)args;
+  
+  long thread_id = arguments.thread_id;
+  int product_id = arguments.product_id;
+  int quantity = arguments.quantity;
+  
+  time_now();
+  // Thread start
+  
+  printf("+++ Thread OF WRITE tid[%ld] invoked!! %d:%d:%d\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec);
+  // sem_wait(&stock_semaphore);
+  stock_mutex.lock();
+
+  printf(ANSI_COLOR_GREEN "[SYS][WRITE]->" ANSI_COLOR_RESET "TID[%ld] starting!!\n\t** ENTERING CRITIC REGION !!** %d:%d:%d\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec);
+  // sleep((rand() % 3 + 1));
+  sleep(4);
+  
+  time_now();
+  printf(ANSI_COLOR_YELLOW "[RUNNING][WRITE]->" ANSI_COLOR_RESET "TID[%ld] %d:%d:%d\n\tWriting data from PRODUCT_ID=[%d]. AMOUNT=[%d] -> [%d]\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec, product_id, stock[product_id].amount, stock[product_id].amount + quantity);
+  stock[product_id].amount += quantity;
+  
+  time_now();
+  printf(ANSI_COLOR_RED "[SYS][WRITE]->" ANSI_COLOR_RESET "TID[%ld] finished!!\n\t** LEFTING  CRITIC REGION !!** %d:%d:%d\n", thread_id, timeinfo->tm_hour, timeinfo->tm_hour, timeinfo->tm_sec);
+  
+  // sem_post(&stock_semaphore);
+  stock_mutex.unlock();
+  
+  pthread_exit(NULL);   
+}
+
+int main() {
+  /* 
+    O tema abordado será um estoque de uma distribuidora.
+    Cada loja compradora de Product será uma thread.
+  */
+  srand(time(NULL));
+  int stopper_flag;
+  int product_target = 1;
+  int readers;
+  int writers;
+  cin >> readers >> writers;
+  printf("\nNumber of readers: %d\tNumber of writers: %d\n\n", readers, writers);
+
+  int max_threads = readers + writers;
+  pthread_t stores[max_threads];
+  int writes_op[writers]; // values of change the amount
+
+  for (int i = 0; i < writers; ++i) {
+    cin >> writes_op[i];
+    // cout << writes_op[i] << endl;
+  }
+
+  vector<int> shuffled_ops;
+  for (int i = 0; i < writers; ++i) {
+    shuffled_ops.push_back(writes_op[i]);
+  }
+  for (int i = 0; i < readers; ++i) {
+    shuffled_ops.push_back(0);
+  }
+  random_shuffle ( shuffled_ops.begin(), shuffled_ops.end() );
+
+  // for (int i = 0; i < shuffled_ops.size(); ++i) {
+  //   cout << shuffled_ops[i] << " ";
+  // }
+  // cout << endl;
+  initStock();
+  printStock();
+
+  printf("\n**** starting main ****\n\n");
+  Thread_Args args;
+
+  int tid = 0; // var to control thread id through w and r
+
+  for(int i = 0; i < shuffled_ops.size(); ++i) {
+    if (shuffled_ops[i] == 0) {
+      args = {
+        tid,
+        product_target,
+      };
+      stopper_flag = pthread_create(&stores[tid], NULL, reader, &args);
+    } else {
+      args = {
+        tid,
+        product_target,
+        shuffled_ops[i], 
+      };
+      stopper_flag = pthread_create(&stores[tid], NULL, writer, &args);
+    }
+    if (stopper_flag != 0) {
+      cout << "Error creating thread " << tid << ". Return code:" << stopper_flag <<  endl;
+    }
+    tid++;
+    sleep(0.0005);
+  }
+
+  for(int i = 0; i < max_threads; i++) {
+    pthread_join(stores[i], NULL);
+  }
+  cout << "All threads completed." << endl;
+
+  printf("\n**** ending main ****\n\n");
+  printStock();
+  return 0;
+}
